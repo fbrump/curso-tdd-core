2aec88e70642f1eb9512d2fdf735cc6758ae7977
Felipe Brum <fbrump@gmail.com>
2017-02-11 21:29:17 -0200

    Added comments in class Lance.cs

--- src/Caelum.Leilao/Lance.cs
+++ src/Caelum.Leilao/Lance.cs
@@ -1,10 +1,26 @@
 namespace Caelum.Leilao
 {
+    /// <summary>
+    /// This class mapping throw class.
+    /// </summary>
     public class Lance
     {
+        /// <summary>
+        /// User that give throw
+        /// </summary>
+        /// <returns>User.</returns>
         public Usuario Usuario { get; set; }
+        /// <summary>
+        /// Value of the throw.
+        /// </summary>
+        /// <returns>Value</returns>
         public double Valor { get; set; }
 
+        /// <summary>
+        /// Method that do throw.
+        /// </summary>
+        /// <param name="usuario">User give throw.</param>
+        /// <param name="valor">Value of the throw.</param>
         public Lance(Usuario usuario, double valor)
         {
             this.Usuario = usuario;
