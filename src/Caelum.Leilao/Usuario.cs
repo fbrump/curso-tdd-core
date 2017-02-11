a24c017c3b5471ab6a0f9de11a5c3b83ea197a39
Felipe Brum <fbrump@gmail.com>
2017-02-11 21:36:13 -0200

    Added coments on Usuario.cs

--- src/Caelum.Leilao/Usuario.cs
+++ src/Caelum.Leilao/Usuario.cs
@@ -1,16 +1,36 @@
 namespace Caelum.Leilao
 {
+    /// <summary>
+    /// This class mapping user.
+    /// </summary>
     public class Usuario
     {
+        /// <summary>
+        /// Identified of the User.
+        /// </summary>
+        /// <returns>ID</returns>
         public int Id { get; set; }
+        /// <summary>
+        /// Name of the User
+        /// </summary>
+        /// <returns>Name</returns>
         public string Nome { get; set; }
 
+        /// <summary>
+        /// Construction fo the user.
+        /// </summary>
+        /// <param name="nome">Name of the User.</param>
         public Usuario(string nome)
             : this(0, nome)
         {
             
         }
 
+        /// <summary>
+        /// Construction of the user.
+        /// </summary>
+        /// <param name="id">ID of the user.</param>
+        /// <param name="nome">Name of the user.</param>
         public Usuario(int id, string nome)
         {
             this.Id = id;
